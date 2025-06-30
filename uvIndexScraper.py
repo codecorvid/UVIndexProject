e89fd03ec26c27c0f09e9440ab67d930a667437d
codecorvid <99363696+codecorvid@users.noreply.github.com>
2025-06-30 10:02:26 -0700

    Get full year's data
    - fixed to actually access each month's data

--- uvIndexScraper.py
+++ uvIndexScraper.py
@@ -5,7 +5,11 @@ import csv
 
 start = time.time()
 
-monthDays = {"Jan" : 31, "Feb" : 29}
+monthDays = {"Jan" : 31, "Feb" : 29, "Mar" : 31, "Apr" : 30, "May" : 31, "Jun" : 30, 
+             "Jul" : 31, "Aug" : 31, "Sep" : 30, "Oct" : 31, "Nov" : 30, "Dec" : 31}
+
+numericMonth = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May" : "05", "Jun" : "06", 
+             "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
 
 with open("DailyUVIndexData2020.csv", "w", newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
@@ -15,13 +19,14 @@ with open("DailyUVIndexData2020.csv", "w", newline='') as csv_file:
         print(month)
         #get month's number of days
         numDays = monthDays[month]
+        numMonth = numericMonth[month]
 
         for day in range(1, numDays + 1):
             print(day)
             if day < 10:
-                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.2020010' + str(day) + '12.uvbull'
+                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.2020' + numMonth + '0' + str(day) + '12.uvbull'
             if day >= 10:
-                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.202001' + str(day) + '12.uvbull'
+                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.2020' + numMonth + str(day) + '12.uvbull'
 
             req = requests.get(url)
             soup = BeautifulSoup(req.content, 'html.parser')
