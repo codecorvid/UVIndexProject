d0c6071d2700ceea8ff0608657a9b0a9688d73ae
codecorvid <99363696+codecorvid@users.noreply.github.com>
2025-06-30 09:34:28 -0700

    Add files via upload
    - removed average calculation
    - can now get data for multiple months

--- uvIndexScraper.py
+++ uvIndexScraper.py
@@ -3,35 +3,35 @@ from bs4 import BeautifulSoup
 import time
 import csv
 
-sum = 0
-
 start = time.time()
+
+monthDays = {"Jan" : 31, "Feb" : 29}
 
 with open("DailyUVIndexData2020.csv", "w", newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(["Month", "Day", "UVIndex"])
 
-    #first go, loop through just the month of January
-    for i in range(1, 32):
-        print(i)
-        if i < 10:
-            url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.2020010' + str(i) + '12.uvbull'
-        if i >= 10:
-            url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.202001' + str(i) + '12.uvbull'
+    for month in monthDays:
+        print(month)
+        #get month's number of days
+        numDays = monthDays[month]
 
-        req = requests.get(url)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        indexAZ = soup.text.find('AZ')
-        currIndex = soup.text[indexAZ + 6:indexAZ + 8]
+        for day in range(1, numDays + 1):
+            print(day)
+            if day < 10:
+                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.2020010' + str(day) + '12.uvbull'
+            if day >= 10:
+                url = 'https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/Bulletin/2020/uv.202001' + str(day) + '12.uvbull'
 
-        #writing to CSV file
-        csv_writer.writerow(["Jan", str(i), currIndex])
+            req = requests.get(url)
+            soup = BeautifulSoup(req.content, 'html.parser')
+            indexState = soup.text.find('AZ')
+            currIndex = soup.text[indexState + 6:indexState + 8]
 
-        sum += int(currIndex)
-        #print(currIndex) #get the UV Index value for PHX, AZ accounting for double digits
+            #writing to CSV file
+            csv_writer.writerow([month, str(day), currIndex])
+
 
 end = time.time()
 
-average = sum/31
-print(average)
 print((end - start), ' seconds')
